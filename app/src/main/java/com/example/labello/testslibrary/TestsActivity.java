6ead5a345e0a5218509377cfd01d87eede69f1dc
AhmedMaad <atef.ahmed1341@gmail.com>
2021-06-11 15:03:03 +0200

    searhing in test library is done

--- app/src/main/java/com/example/labello/testslibrary/TestsActivity.java
+++ app/src/main/java/com/example/labello/testslibrary/TestsActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import com.example.labello.R;
 
@@ -11,12 +14,15 @@ import java.util.ArrayList;
 
 public class TestsActivity extends AppCompatActivity {
 
+    private ArrayList<TestsLibraryModel> models = new ArrayList<>();
+    private ArrayList<TestsLibraryModel> searchList = new ArrayList<>();
+    private TestsLibraryAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tests);
 
-        ArrayList<TestsLibraryModel> models = new ArrayList<>();
         models.add(new TestsLibraryModel("Complete Blood Cell (CBD)", R.drawable.cbc));
         models.add(new TestsLibraryModel("Hemoglobin (A1c)", R.drawable.hemoglobin));
         models.add(new TestsLibraryModel("Liver Function Test (LFT)", R.drawable.liver));
@@ -28,9 +34,34 @@ public class TestsActivity extends AppCompatActivity {
         models.add(new TestsLibraryModel("Blood & Urine Test (BUT)", R.drawable.blood_urine));
         models.add(new TestsLibraryModel("Fast Blood Glucode Level", R.drawable.glucose));
 
-        TestsLibraryAdapter adapter = new TestsLibraryAdapter(this, models);
+        adapter = new TestsLibraryAdapter(this, models);
         RecyclerView rv = findViewById(R.id.rv);
         rv.setAdapter(adapter);
+
+        EditText searchET = findViewById(R.id.et_search);
+        searchET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                searchList.clear();
+                for (TestsLibraryModel test : models) {
+                    if (test.getName().toLowerCase().contains(s.toString().toLowerCase())) {
+                        searchList.add(test);
+                    }
+                }
+                adapter = new TestsLibraryAdapter(TestsActivity.this, searchList);
+                rv.setAdapter(adapter);
+            }
+        });
 
     }
 
